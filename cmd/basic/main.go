48edbdf1ca84c9f4184ab8f1571be2d84d17260d
anzboi <daniel.cantos@anz.com>
2020-03-28 12:40:04 +1100

    read port environment variable

--- cmd/basic/main.go
+++ cmd/basic/main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/anzboi/basic-grpc/pkg/svc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-var (
-	port = flag.Int("port", 8080, "port to listen on")
-)
-
 func main() {
-	flag.Parse()
-	port := *port
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("no PORT env variable, defaulting to 8080")
+		port = "8080"
+	}
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		panic(err)
 	}
 	svr := grpc.NewServer()
 	svc.RegisterServiceServer(svr, Svc{})
 	reflection.Register(svr)
-	log.Printf("listening on port %d", port)
+	log.Printf("listening on port %s", port)
 	log.Fatal(svr.Serve(lis))
 }
